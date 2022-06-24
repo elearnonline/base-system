3efd260992b0c9c9a76243dd451f0d44bbd46cb9
James Eco <elearnnow1@gmail.com>
2022-06-24 17:22:50 +0300

    base with auth0 and form with mongo database

---
+++ pages/allmeetups/index.js
@@ -0,0 +1,41 @@
+import { Fragment } from 'react';
+import { MongoClient } from 'mongodb';
+
+import MeetupList from '../../components/meetups/MeetupList';
+
+function MeetupsList(props) {
+  return (
+    <Fragment>
+      <MeetupList meetups={props.meetups} />;
+    </Fragment>
+  );
+}
+
+export async function getStaticProps() {
+  // fetch data from an API
+  const client = await MongoClient.connect(
+    'mongodb+srv://jameseco:MySons**20177@cluster0.irurs.mongodb.net/?retryWrites=true&w=majority'
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
+}
+
+export default MeetupsList;
+
---
+++ pages/api/new-meetup.js
@@ -0,0 +1,30 @@
+import { MongoClient } from 'mongodb';
+import {  withPageAuthRequired } from '@auth0/nextjs-auth0';
+
+// /api/new-meetup
+// POST /api/new-meetup
+
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      'mongodb+srv://jameseco:MySons**20177@cluster0.irurs.mongodb.net/?retryWrites=true&w=majority'
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
+  }
+}
+
+export default handler;
+
+export const getServerSideProps = withPageAuthRequired();
